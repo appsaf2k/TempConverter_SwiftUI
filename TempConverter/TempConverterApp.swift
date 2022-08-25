9ad40b0627ad575ef0c08afdd378ba1171c77212
appsaf2k <appsaf2k@gmail.com>
2022-08-25 10:07:08 +0300

    Initial Commit

---
+++ TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by @andreev2k on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
